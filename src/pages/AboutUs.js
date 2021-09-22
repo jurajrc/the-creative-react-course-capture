246c94ec9fc79e029eef03254d65b2dc5d2da1c4
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-09-22 10:38:15 +0200

    About Section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// imoport Components
+import AboutSection from '../conponents/AboutSection';
+
+const AboutUs = () => {
+    return (
+        <AboutSection />
+    )
+}
+
+export default AboutUs
