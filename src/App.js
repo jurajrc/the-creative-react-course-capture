e785120df3f734223747bf828b2b51786d7c079c
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-09-22 19:27:47 +0200

    8.  create and styled Nav

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import GlobalStyle from "./conponents/GlobalStyle";
 
 // import Components
 import AboutUs from "./pages/AboutUs";
+import Nav from "./conponents/Nav";
 
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
