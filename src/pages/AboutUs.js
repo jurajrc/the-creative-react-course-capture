6e3f501dbb1bcdd1374a54efc125441e1f8db936
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-09-22 11:41:33 +0200

    4. Faq Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,13 +1,16 @@
 import React from 'react';
+
 // imoport Components
 import AboutSection from '../conponents/AboutSection';
 import ServicesSection from '../conponents/ServicesSection';
+import FaqSection from '../conponents/FaqSection';
 
 const AboutUs = () => {
     return (
         <>
             <AboutSection />
             <ServicesSection />
+            <FaqSection />
         </>
     )
 }
